33cc37125c68ee279c6ca773d30cd5621ba3ca92
Hatim Shabbir <hatim.shabbir5152@gmail.com>
2024-02-04 16:57:55 +0100

    Updated: requirements.txt

--- models/products.py
+++ models/products.py
@@ -1,14 +1,14 @@
-from sqlalchemy import Column, Integer, String, Float
-from sqlalchemy.ext.declarative import declarative_base
+# # from sqlalchemy import Column, Integer, String, Float
+# # from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+# Base = declarative_base()
 
-class Product(Base):
-    __tablename__ = 'products'
+# class Product(Base):
+#     __tablename__ = 'products'
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    name = Column(String, index=True)
-    production_location = Column(String)
-    price = Column(Float)
-    production_date = Column(String)
-    block_hash = Column(String)
+#     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+#     name = Column(String, index=True)
+#     production_location = Column(String)
+#     price = Column(Float)
+#     production_date = Column(String)
+#     block_hash = Column(String)
